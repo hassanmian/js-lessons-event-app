0320cc71344069786068f98e743ea855fe0a6688
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-10 11:33:34 +0200

    Start work with login logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
+const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`
+
 function App() {
 
   function login() {
-    console.log("login")
+    fetch(LOGIN_URL)
   }
 
   return (
