3cff57a1a004aeb23b5accad0dd94e85cf8c230f
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-10 14:08:45 +0200

    Add routes for EventListPage and LoginPage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@
 // import React from 'react';
 // import { useRef, useState } from 'react';
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
 import LoginPage from './pages/LoginPage';
+import EventListPage from './pages/EventListPage';
 
 function App() {
 
   return (
     <div>
-      <LoginPage />
+      <Switch>
+
+        <Route path="/event-list">
+          <EventListPage />
+        </Route>
+
+        <Route path="/">
+          <LoginPage />
+        </Route>
+        
+      </Switch>
+      
     </div>
   );
 }
